e6f24ebc1f111d2d52113810a071fd62b8df1a70
Zack Liscio <zackliscio@gmail.com>
2013-01-10 19:17:37 -0800

    fixed heroku flaw

--- app/assets/javascripts/bingo.js
+++ app/assets/javascripts/bingo.js
@@ -1,17 +1,14 @@
 $(document).ready(function(){
    
-
-
-    var number = 0; 
-    // var number = "hipster";
-
     var resetWord = "filler";
    
-    var safariArray = new Array("hipster","fupa","muffin top","fixed gear bicycle","VW Bug","test","motorcycle","nose-picking","suit","red shirt","political shirt","messy eater");
-    var usedSafariArray = {"hipster":false,"fupa":false,"muffin top":false,"fixed gear bicycle":false,
-    "VW Bug":false,"test":false,"motorcycle":false,"nose-picking":false,"suit":false,"red shirt":false,"political shirt":false,"messy eater":false};
+    var safariArray = new Array("hipster","fupa","muffin top","fixed gear bicycle","VW Bug","parking fail","motorcycle",
+        "nose-picking","suit","red shirt","political shirt","messy eater","nerd","crying baby","soccer mom");
+    var usedSafariArray = {"hipster":false,"fupa":false,"muffin top":false,"fixed gear bicycle":false,"VW Bug":false,"parking fail":false,"motorcycle":false,
+    "nose-picking":false,"suit":false,"red shirt":false,"political shirt":false,"messy eater":false,"nerd":false,"crying baby":false,"soccer mom":false};
 
     init();
+
    
     function init(){
         for(var i = 0; i<18; i++){
@@ -20,13 +17,8 @@ $(document).ready(function(){
     }
        
     function fillCard(i){
-
-
-        
         wordBase = Math.floor(Math.random() * (safariArray.length));
-
         word = safariArray[wordBase];
-     
         if(usedSafariArray[word] != true){
             $('#cell'+i).html(word);
             usedSafariArray[word] = true;
@@ -34,17 +26,14 @@ $(document).ready(function(){
             fillCard(i);
         }
     }
-   
-    // function resetUsedSafariArray(){
-    //     for(var j = 0; j < SafariArray.length; j++){
-    //         resetWord = SafariArray[j];
+    
 
-    //         usedSafariArray[resetWord] = false;
-    //     }
-    // }
+
     function resetUsedSafariArray(){
         alert("hipster");
     }
  
 
+
 });
+
